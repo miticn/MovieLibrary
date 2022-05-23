eff45e91d05bd20ef0cb7fb8ee1c5518eb23325a
miticn <101733519+miticn@users.noreply.github.com>
2022-05-23 02:09:57 +0200

    Prebacene neke stavke u BaseController i otpocet search

--- Implementacija/app/Http/Controllers/BaseController.php
+++ Implementacija/app/Http/Controllers/BaseController.php
@@ -6,12 +6,40 @@ use App\Models\FilmModel;
 use App\Models\KorisnikModel;
 
 class BaseController extends Controller{
-    
+    private static function getTrophy($score){
+        if($score<50){
+            $trophy = 'fa-recycle trash';
+        }else if ($score>=50 && $score<66){
+            $trophy = 'fa-trophy bronze-trophy';
+        }else if ($score>=66 && $score<82){
+            $trophy = 'fa-trophy silver-trophy';
+        }else if ($score>=82){
+            $trophy = 'fa-trophy gold-trophy';
+        }
+        return $trophy;
+    }
+    private static function getScore($BrojLajk,$BrojDislajk){
+        return round($BrojLajk/($BrojLajk+$BrojDislajk)*100);
+    }
+
+
     public function profile($id){
         return view('profile', ['profile' => KorisnikModel::find($id)]);
     }
 
     public function indexPage(){
-        return view('index',['filmovi' => FilmModel::orderByDesc('BrojLajk')->limit(18)->get()]);
+        $filmovi = FilmModel::orderByDesc('BrojLajk')->limit(18)->get();
+        $trophies = [];
+        $scores = [];
+        foreach($filmovi as $film){
+            $score = BaseController::getScore($film->BrojLajk,$film->BrojDislajk);
+            array_push($scores,$score);
+            array_push($trophies,BaseController::getTrophy($score));
+        }
+        
+        return view('index',['filmovi' => $filmovi, 'scores'=>$scores, 'trophies'=>$trophies]);
+    }
+    public function search($Naziv){
+
     }
 }
