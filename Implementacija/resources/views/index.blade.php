daef2caf69757c7cecfb3979b4997e0b484b6c96
miticn <101733519+miticn@users.noreply.github.com>
2022-05-24 21:06:18 +0200

    Dodavanje bootstrapa u template i poboljsanje indexA

--- Implementacija/resources/views/index.blade.php
+++ Implementacija/resources/views/index.blade.php
@@ -1,10 +1,19 @@
 @extends('template')
 @section('content')
 <h1>Aktuelni Filmovi:</h1>
-		<ul class="new-popular-movies">
-			@for ($i = 0; $i < count($filmovi); $i++)
-				<li class="new-popular-movies"><a class="new-popular-movies" href="movie/{{$filmovi[$i]->idFilm}}"><img class="movie-banner" src="IMG/Batman-2022.jpg"><br><b>{{$filmovi[$i]->Naziv}}</b><br> <i class="fa {{$trophies[$i]}}"></i> {{$scores[$i]}}% </a></li>
-			@endfor	
-		</ul>
+		@for ($j =0; $j<3; $j++)
+			<div class="row">
+				@for ($i = $j*6; $i < ((count($filmovi)<($j+1)*6)?count($filmovi):($j+1)*6); $i++)
+					<div class="col-sm-2">
+						<a class="new-popular-movies" href="movie/{{$filmovi[$i]->idFilm}}">
+							<img class="movie-banner" src="IMG/Batman-2022.jpg"><br>
+							<b>{{$filmovi[$i]->Naziv}}</b><br> 
+							<i class="fa {{$trophies[$i]}}"></i> {{$scores[$i]}}% 
+						</a>
+					</div>
+				@endfor	
+			</div>
+		@endfor
+		
 
 @endsection
