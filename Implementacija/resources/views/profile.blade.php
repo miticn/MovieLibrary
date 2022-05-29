29c62cdb12df7b08c63ba73ec7b50934f8727dfc
Mateja Milojević <matosmatke@gmail.com>
2022-05-29 21:15:23 +0200

    Uradjen stajling za profil, dodat profile.css za tu upotrebu samo.

--- Implementacija/resources/views/profile.blade.php
+++ Implementacija/resources/views/profile.blade.php
@@ -1,8 +1,8 @@
 @extends('template')
 @section('content')
-
+<link rel="stylesheet" href="{{ URL::asset('css/profile.css') }}">
 <table class="profile">
-    <tr>
+    <tr class="border">
         <td class="profile_slika">
             <div>
                 <img class="profile_slika" src="{{URL::asset('IMG/img_profile/profile'.$profile->idKorisnik.'.png')}}"
@@ -13,7 +13,7 @@
             <table id="ime_opis">
                 <tr id="profile_ime">
                     <td>
-                        {{$profile->Ime}}
+                        &nbsp;&nbsp;&nbsp;{{$profile->Ime}}
                     </td>
                     @auth    
                     <td style="text-align: right; width:50px;">
@@ -28,8 +28,8 @@
                     @endauth
                 </tr>
                 <tr id="profile_opis"> 
-                    <td colspan='2'>
-                        {{$profile->Opis}}
+                    <td colspan='2' style="">
+                        <i>  "{{$profile->Opis}}" </i>      
                     </td>
                 </tr>
             </table>
@@ -43,12 +43,12 @@
         <table>
         @auth
         @if($profile->idKorisnik == Auth::id())
-        <tr>
+        <tr style="background-color: #2f2f2f">
             <form name="napravi_listu" action="{{route('napravi_listu')}}" method="GET">
                 <td>
                     <input type="submit" value="Napravi listu">
                 </td>
-                <td>
+                <td id="input_lista">
                     <input type="text" name="Ime">
                 </td>
             </form>
@@ -56,51 +56,55 @@
         @endif
         @endauth
         @foreach ($profile->liste as $lista)
-        <tr>@auth
-            <td style="text-align: center;">
+        <tr class="listic"><td class="listic" colspan="2">
+        <a href="{{route('lista', ['id' => $lista->idLista])}}">
+        
+        <div class="listcard" style="overflow-y: hidden;">
+            <div class="listborder">
+                <p><b>{{$lista->Ime}}</b></p>
+            </div>
+            <div>@auth
                 @if($profile->idKorisnik == Auth::id())
                 <form name="zaboravi" action="{{route('zaboravi_listu', ['id' => $lista->idLista])}}"
                     method="GET"><input type="submit" value="zaboravi"></form>
                 @else
                 <form name="sacuvaj" action="{{route('sacuvaj_listu', ['id' => $lista->idLista])}}"
-                     method="GET"><input type="submit" value="sacuvaj"></form>
+                    method="GET"><input type="submit" value="sacuvaj"></form>
                 @endif
-            </td>
-            @endauth
-            <td style="width: 100%;">
-                <a href="{{route('lista', ['id' => $lista->idLista])}}">
-                <p class="lista" style="background-image: url('{{URL::asset('IMG/img_lista/lista'.$lista->idLista.'.jpg')}}')">
-                    {{$lista->Ime}}
-                </p>
-                </a>
-            </td>
-            <td>
-                lajkovi
-            </td>
-        </tr>
+                <form name="oceni"><input type="submit" value="oceni"></form>
+                @endauth
+                <form style="color: white; font-size: 20px;">2452346</form>
+            </div>
+            </div>
+        </a>
+        </td></tr>
         @endforeach
         </table>
         </div>
     </td>
-    <td id="profili">
-        <table>
+    <td>
+        <div id="profili">
+        <table id="profili_sacuvani">
         @foreach ($profile->sacuvani as $sacuvan)
-        <tr>
-            <td><a id="sacuvani_link" href="{{route('profile', ['id' => $sacuvan->idKorisnik])}}">
+        <tr class="profili">
+            <td colspan="2"><a href="{{route('profile', ['id' => $sacuvan->idKorisnik])}}">
                 <img class="sacuvani_slika" src="{{URL::asset('IMG/img_profile/profile'.$sacuvan->idKorisnik.'.png')}}"
                 onerror="this.onerror=null; this.src='{{URL::asset('IMG/img_profile/profile_def.jpg')}}'">
                 {{$sacuvan->Ime}}</a>
             </td>
-            @if($profile->idKorisnik == Auth::id())
             <td>
+                @auth
+                @if($profile->idKorisnik == Auth::id())
                 <form name="zaboravi" action="{{route('zaboravi_korisnika', ['id' => $sacuvan->idKorisnik])}}"
                     method="GET"><input type="submit" value="x">
                 </form>
+                @endif
+                @endauth
             </td>
-            @endif
         </tr>
         @endforeach 
         </table>
+        </div>
     </td>
 </tr></table>
 
