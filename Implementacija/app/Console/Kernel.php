b095679ffcf4ab8469b76bebab821c4bff776aff
miticn <101733519+miticn@users.noreply.github.com>
2022-05-30 01:01:39 +0200

    Schedule promenjen na jednom u sat vremena

--- Implementacija/app/Console/Kernel.php
+++ Implementacija/app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         $schedule->call(function () {
             CineplexxController::updateRepertoar();
-        })->everyMinute();
+        })->hourly();
     }
 
     /**
