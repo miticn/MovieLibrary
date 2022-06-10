414a61fb001b4a20796103b5b7e6d2121ddf4f11
miticn <101733519+miticn@users.noreply.github.com>
2022-06-10 16:55:56 +0200

    Example test case

--- Implementacija/tests/Feature/ExampleTest.php
+++ Implementacija/tests/Feature/ExampleTest.php
@@ -2,8 +2,12 @@
 
 namespace Tests\Feature;
 
+use App\Http\Controllers\KorisnikController;
+use App\Models\KorisnikModel;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Request;
 use Tests\TestCase;
+use Illuminate\Support\Facades\Auth;
 
 class ExampleTest extends TestCase
 {
@@ -12,10 +16,22 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    public function test1(){
+        $imeToSet = "Novo ime Admina";
+        $opisToSet = "Novi opis Admina";
+        $idKorisnika = 1;
 
-        $response->assertStatus(200);
+        $kc = new KorisnikController();
+        $mockRequest = $this->createStub(Request::class);
+        $mockRequest->Ime = $imeToSet;
+        $mockRequest->Opis = $opisToSet;
+        Auth::shouldReceive('id')->andReturn($idKorisnika);
+
+        $kc->izmeni_submit($mockRequest);
+
+        $korisnik = KorisnikModel::find($idKorisnika);
+
+        $this->assertEquals($korisnik->Ime,$imeToSet);
+        $this->assertEquals($korisnik->Opis,$opisToSet);
     }
 }
